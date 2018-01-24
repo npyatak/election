5d9f8038b1f4e07224d9a16fea0ab49c9d152e86
Nick <pyatakn@gmail.com>
2018-01-24 16:19:00 +0300

    upd

--- console/migrations/m180115_161527_create_table_rating_item.php
+++ console/migrations/m180115_161527_create_table_rating_item.php
@@ -23,6 +23,22 @@ class m180115_161527_create_table_rating_item extends Migration {
         $this->addForeignKey("{rating_item}_candidate_id_fkey", '{{%rating_item}}', 'candidate_id', '{{%candidate}}', 'id', 'CASCADE', 'CASCADE');
         $this->addForeignKey("{rating_item}_rating_id_fkey", '{{%rating_item}}', 'rating_id', '{{%rating}}', 'id', 'CASCADE', 'CASCADE');
         $this->addForeignKey("{rating_item}_rating_group_id_fkey", '{{%rating_item}}', 'rating_group_id', '{{%rating_group}}', 'id', 'CASCADE', 'CASCADE');
+
+        $this->batchInsert('{{%rating_item}}', ['candidate_id', 'additional_id', 'rating_group_id', 'rating_id', 'score'], [
+            [1, null, 1, 1, 74.8],
+            [2, null, 1, 1, 5.3],
+            [null, 1, 1, 1, 0.6],//'Приду и испорчу бюллетень',
+            [null, 2, 1, 1, 5.7],//'Не стал бы участвовать в выборах',
+            [null, 3, 1, 1, 2],//'Другой',
+            [null, 4, 1, 1, 5.7],//'Затрудняюсь ответить',
+            
+            [1, null, 3, 1, 69.1],
+            [2, null, 3, 1, 6.9],
+            [null, 1, 3, 1, 0.6],
+            [null, 2, 3, 1, 6.5],
+            [null, 3, 3, 1, 2.9],
+            [null, 4, 3, 1, 5.5]
+        ]);
     }
 
     public function safeDown()
