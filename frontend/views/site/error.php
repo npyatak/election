0645f59b046fc7813f5aa04e2ecc11c05d423118
Nick <pyatakn@gmail.com>
2018-02-13 20:10:01 +0300

    upd

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -6,21 +6,21 @@
 
 /* @var $exception Exception */
 
+use yii\helpers\Url;
 use yii\helpers\Html;
 
 $this->title = $name;
+$this->context->layout = 'error';
+
+$this->registerCssFile(Url::toRoute('css/404.css'));
 ?>
-<div class="site-error">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
+<div class="not-found">
+    <h1 class="not-found__title">Страница не найдена</h1>
+    <p class="not-found__subtitle">
+        Такой страницы не существует. Возможно неверно указан адрес или страница удалена.
     </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+    <a href="<?=Url::home();?>" class="btn btn-green btn-h40 btn-w200" style="font-size: 16px;">
+        На главную
+    </a>
 </div>
